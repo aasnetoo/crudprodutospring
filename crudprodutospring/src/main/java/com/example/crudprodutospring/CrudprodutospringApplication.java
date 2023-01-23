a3d81f663d152082d8e8ab56e64f94e061ed962e
Antonio Neto <antonioneto.mec@gmail.com>
2023-01-23 16:54:27 -0300

    Initial commit

---
+++ crudprodutospring/src/main/java/com/example/crudprodutospring/CrudprodutospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudprodutospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudprodutospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudprodutospringApplication.class, args);
+	}
+
+}
---
+++ crudprodutospring/src/test/java/com/example/crudprodutospring/CrudprodutospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudprodutospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprodutospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
